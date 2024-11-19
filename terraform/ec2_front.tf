833140ac129b5f2cd04974f9cbf9a15226281b6e
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-11-19 14:48:18 +0100

    Install Docker on EC2

--- terraform/ec2_front.tf
+++ terraform/ec2_front.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "ec2_front_instance" {
   instance_type          = local.instance_type
   vpc_security_group_ids = [aws_security_group.vpc_security_group.id]
   key_name               = "vockey"
+
+  user_data_replace_on_change = true
+  user_data                   = <<-USERDATAEOF
+  #!/bin/env bash
+
+  ${locals.ec2_common_setup}
+  USERDATAEOF
+
   tags = {
     Name = "CloudP1 EC2 Frontend"
   }
